ca641e0f0978d918b741becc027d1d5d48e789ac
Kacper <kacpergierycz@gmail.com>
2021-01-28 16:26:13 +0100

    list of customers from model using th:each and th:text

---
+++ 32-thymeleafdemo-employees-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,55 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springboot.thymeleafdemo.model.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	// load employee data
+	
+	private List<Employee> theEmployees;
+	
+	@PostConstruct
+	private void loadData() {
+		
+		// create employees
+		System.out.println("filling list.....");
+		Employee emp1 = new Employee(1, "Leslie", "Andrews", "leslieAndrews@luv2code.com");
+		Employee emp2 = new Employee(2, "Emma", "Baum", "emmaBaum@luv2code.com");
+		Employee emp3 = new Employee(3, "Avani", "Gupta", "avaniGupta@luv2code.com");
+		
+		// create the list
+		theEmployees = new ArrayList<>();
+		
+		// add to the list
+		theEmployees.add(emp1);
+		theEmployees.add(emp2);
+		theEmployees.add(emp3);
+		
+		
+	}
+	
+	
+	// add mapping for "/list"
+	
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+	}
+	
+}
